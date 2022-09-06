01dc5b2984b6fb4e4d1f50399d0ce4aa575a0e48
Marina <bmv1202@gmail.com>
2022-09-06 10:58:19 +0300

    добавили все папки

---
+++ 123/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, каторая на вход принимает два числа и выдает какое большее, а какое меньшее
+
+        {
+            Console.WriteLine("Вводим 2 числа и узнаем какое из них больше");
+            int a, b;
+            Console.WriteLine("Вводим первое число - ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вводим второе число - ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            bool Chto0 = a > b;
+            bool Chto1 = a == b;
+            bool Chto2 = a < b;
+
+            if (Chto0)
+            {
+                Console.WriteLine($"Число {a} больше чем {b}");
+            }
+            else if (Chto1)
+            {
+                Console.WriteLine($"Числа {a} и {b} равны");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} больше чем {a}");
+            }}
---
+++ 12345/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число, a ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число, b ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число, c ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.Write($"a = {a}, b = {b}, c = {c} -> max = {a} ");
+}
+else if (b >c)
+    {
+        Console.Write($"a = {a}, b = {b} -> max = {b} ");
+    }
+    else
+    {
+        Console.Write($"a = {a}, b = {b} -> max = {c} ");
+    }
